ee06a1e6c260560bdaece93fcc66c7a5d62dab48
Philip Ashlock <phil@civicagency.org>
2012-12-10 10:40:10 -0500

    Adding some intro and overview text

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -73,6 +73,37 @@
 
 	<div id="body">
 		<p><a class="btn-auth btn-github large" href="/login">Sign in with <b>GitHub</b></a></p>        
+		
+		<p>RESTdb allows you to automatically turn any CSV or database into an API</p>
+
+		<h4 style="margin-top : 4em">Details</h4>
+	    <ul>
+			<li><a href="https://github.com/philipashlock/RESTdb">Source Code</a></li>			
+	        <li>File Upload widget with multiple file selection, drag&amp;drop support, progress bars and preview images for jQuery.</li>
+	        <li>Front-end can be used with any server-side platform (PHP, Python, Ruby on Rails, Java, Node.js, Go etc.) that supports standard HTML form file uploads. </li>		
+			<li>Current backend utilizes SQLite for each csv uploaded, DB-to-API for arbitrary queries using GET, and CI REST Server for API logging, 
+				key management, format transformations and other common API features.
+			</li>
+	    </ul>
+
+
+		
+		<h4>This project builds off of the following codebases:</h4>
+        <ul class="nav">
+            <li><a href="https://github.com/blueimp/jQuery-File-Upload/">jQuery File Upload</a></li>
+            <li><a href="https://github.com/project-open-data/db-to-api">DB to API</a></li>
+            <li><a href="https://github.com/philsturgeon/codeigniter-restserver">CI REST Server</a></li>
+        </ul>		
+		
+		<h4>To Do</h4>
+	    <ul>
+	        <li>Implement a real user interface, eg actual do some web design</li>
+		    <li>Implement a form for adding new databases. Backend functionality already works for this </li>
+		    <li>Implement pagination by default and provide some configurable options per database</li>	
+			<li>Implement API management features like API key provisioning. Backend for this mostly in place already</li>		
+	        <li>Auto-generate <a href="http://swagger.wordnik.com/">Swagger</a> schema and show interactive documentation. Allow user to edit some of this per database</li>
+	    </ul>		
+		
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
